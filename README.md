e6c6c66ebdf4aa366164455a2b3dd49cecc72663
osamataqiedeen <otaqiedeen@gmail.com>
2020-09-14 15:39:38 +0300

    Update 3

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # SecuExam.
 This is team SecuExam's submission for the 2020 Amazon Techathon, SecuExam is a tool that allows teachers to hold automatically proctored and cheat proof online exams.  
 This repo will be updated during the techathon (14/9 to 16/9).
-Update 1 (did not start working yet but this is the first three hour push).
--- mainApp.js
+++ mainApp.js
@@ -1,11 +1,46 @@
+//Function that is used as helper to display and 
+const UIHelper = (() => {
+    const showSignUpForm = () => {
+        document.getElementById("signInForm").style.display = 'none';
+        document.getElementById("sign-up-button").style.display = 'none';
+        document.getElementById("signUpForm").style.display = 'block';
+    };
+    const showLoginUpForm = () => {
+        document.getElementById("signInForm").style.display = 'block';
+        document.getElementById("sign-up-button").style.display = 'block';
+        document.getElementById("signUpForm").style.display = 'none';
+    };
+    const hideAuthScreen = () => {
+        document.getElementById("firstScreenContiner").style.display = 'none';
+    }
+    const showTeacherFirstScreen = () => {
+        document.getElementById("mainPageContainer").style.display = "block";
+    }
+    return {
+        showLoginUpForm,
+        showSignUpForm,
+        hideAuthScreen,
+        showTeacherFirstScreen
+    }
+})();
+
 function showSignUpForm(){
-    document.getElementById("signInForm").style.display = 'none';
-    document.getElementById("sign-up-button").style.display = 'none';
-    document.getElementById("signUpForm").style.display = 'block';
+    UIHelper.showSignUpForm();
+}
+
+function submitLogInForm(){
+    UIHelper.hideAuthScreen();
+    UIHelper.showTeacherFirstScreen();
+}
+
+function submitSignUpForm(){
+    UIHelper.hideAuthScreen();
 }
 
 function onStartup(){
     document.getElementById("sign-up-button").addEventListener("click", showSignUpForm);
+    document.getElementById("formSignUp").addEventListener("submit", submitSignUpForm);
+    document.getElementById("formSignIn").addEventListener("submit", submitLogInForm);
 }
 
 window.addEventListener("load", onStartup);
